435884ff12b0c4fae6067831ccf69618ccbbbf7e
ecco257 <connorjwittman@gmail.com>
2023-07-26 14:04:11 -0400

    Edit config to include linkedin

--- app.config.ts
+++ app.config.ts
@@ -2,8 +2,8 @@ export default defineAppConfig({
   // Configure Content Wind here
   cover: '/cover.jpg',
   socials: {
-    twitter: 'Atinux',
-    github: 'Atinux/content-wind'
+    linkedin: 'connor-wittman',
+    github: 'ecco257',
   },
   prose: {
     h1: {
